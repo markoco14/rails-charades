1d27d7d88c32bce1c525a112148f5d7c692584b4
Mark <mark.oconnor14@gmail.com>
2024-11-05 22:16:40 +0800

    create Words controller and boilerplate for view

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "pages#index"  
+  root "pages#index"
+  get "/words", to: "words#index"
 end
