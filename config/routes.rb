4ce4270dcd072ea9df319af6a23948c6a4ae9cab
Mark <mark.oconnor14@gmail.com>
2024-11-05 22:45:56 +0800

    use resources method in routes.rb to create routes needed to manage words

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#index"
-  get "/words", to: "words#index"
-  get "/words/:id", to: "words#show"
+  resources :words
 end
