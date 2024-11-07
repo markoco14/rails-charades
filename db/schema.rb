6c48cdfe2ae4ae4455d5a2a3f33a19087538ec1e
Mark <mark.oconnor14@gmail.com>
2024-11-07 09:43:30 +0800

    update db to postgres
    because putting app in live production environment that doesn't support sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2024_11_05_151020) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "descriptions", force: :cascade do |t|
     t.string "description"
-    t.integer "word_id", null: false
+    t.bigint "word_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["word_id"], name: "index_descriptions_on_word_id"
